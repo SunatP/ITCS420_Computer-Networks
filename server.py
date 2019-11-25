d9f1327eb638a86bcce2b7e02f9021d4a006c5fb
Sunat.P <31818562+SunatP@users.noreply.github.com>
2019-11-25 11:35:40 +0700

    Create server.py

---
+++ server.py
@@ -0,0 +1,15 @@
+import socket
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
+server.bind(('localhost',13245))
+server.listen(5)
+
+while True:
+	print('Waiting for connection . . . ')
+	client, address = server.accept()
+ 	print('... connected from:', address)
+	data = client.recv(1024)
+	if data:
+		client.send(data)
+	client.close()
+server.close()
